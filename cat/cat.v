460883c9599a709fed3545c43b503a45ead28d2b
Flinner <flinner.hlqn@aleeas.com>
2021-07-06 12:44:17 +0300

    refactor(cat): vfmt, my eyes forgive me :(

--- cat/cat.v
+++ cat/cat.v
@@ -17,10 +17,24 @@ struct Settings {
 	show_nonprinting bool
 	show_tabs        bool
 	unbuffered       bool
+	files            []string
 }
 
 fn cat(settings Settings) {
-	println(settings)
+	mut files := settings.files
+
+	// if there are no files, read from stdin
+	if files.len < 1 {
+		files = ['-']
+	}
+
+	for file in files {
+		if file == '-' {
+			content := os.get_raw_lines_joined()
+		} else {
+			content := os.read_file(file) or { panic('No Such File; $file') }
+		}
+	}
 }
 
 fn main() {
@@ -30,21 +44,21 @@ fn main() {
 	fp.description(app_description)
 	fp.skip_executable()
 
-	show_all             := fp.bool('show-all', `A`, false, 'equivalent to -vET')
-	number_nonblanks     := fp.bool('number-nonblank', `b`, false, "Number the lines, but don't count blank lines, override -n")
-	show_ends_and_v      := fp.bool('', `e`, false, 'equivalent to -vE')
-	mut show_ends        := fp.bool('show-ends', `E`, false, 'display $ at end of each line')
-	mut number_all       := fp.bool('number', `n`, false, 'Number the output lines, starting at 1.')
-	squeeze_blank        := fp.bool('sqeeze-blank', `s`, false, 'Squeeze multiple adjacent empty lines, causing the output to be single spaced')
-	mut show_tabs        := fp.bool('', `t`, false, 'Print tab characters as ‘^I’. Implies the -v option to display non-printing characters')
-	show_tabs_and_v      := fp.bool('', `T`, false, 'equivalent to -vT')
+	show_all := fp.bool('show-all', `A`, false, 'equivalent to -vET')
+	number_nonblanks := fp.bool('number-nonblank', `b`, false, "Number the lines, but don't count blank lines, override -n")
+	show_ends_and_v := fp.bool('', `e`, false, 'equivalent to -vE')
+	mut show_ends := fp.bool('show-ends', `E`, false, 'display $ at end of each line')
+	mut number_all := fp.bool('number', `n`, false, 'Number the output lines, starting at 1.')
+	squeeze_blank := fp.bool('sqeeze-blank', `s`, false, 'Squeeze multiple adjacent empty lines, causing the output to be single spaced')
+	mut show_tabs := fp.bool('', `t`, false, 'Print tab characters as ‘^I’. Implies the -v option to display non-printing characters')
+	show_tabs_and_v := fp.bool('', `T`, false, 'equivalent to -vT')
 	// unbuffered        := fp.bool('', `u`, false, 'The output is guaranteed to be unbuffered')
-	unbuffered           := fp.bool('', `u`, false, '(ignored)') // ignored in GNU cat!
+	unbuffered := fp.bool('', `u`, false, '(ignored)') // ignored in GNU cat!
 	mut show_nonprinting := fp.bool('show-nonprinting', `v`, false, 'use ^ and M- notation, except for LFD and TAB')
-	help                 := fp.bool('help', 0, false, 'display this help and exit')
-	version              := fp.bool('version', 0, false, 'output version information and exit')
+	help := fp.bool('help', 0, false, 'display this help and exit')
+	version := fp.bool('version', 0, false, 'output version information and exit')
 
-	additional_args := fp.finalize() or {
+	files := fp.finalize() or {
 		eprintln(err)
 		println(fp.usage())
 		exit(1)
@@ -60,11 +74,11 @@ fn main() {
 	}
 
 	// some flags override each other
-	show_ends        =        show_ends || show_ends_and_v || show_all
-	show_tabs        =        show_tabs || show_tabs_and_v || show_all
+	show_ends = show_ends || show_ends_and_v || show_all
+	show_tabs = show_tabs || show_tabs_and_v || show_all
 	show_nonprinting = show_nonprinting || show_ends_and_v || show_all || show_tabs_and_v
-	number_all       = number_all && !number_nonblanks
+	number_all = number_all && !number_nonblanks
 
 	cat(Settings{number_nonblanks, number_all, squeeze_blank, show_ends, show_nonprinting
-		|| show_ends_and_v, show_tabs, unbuffered})
+		|| show_ends_and_v, show_tabs, unbuffered, files})
 }
