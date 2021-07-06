3cce216ad349d6cbe1b8028417d588f6f5770905
VinWare <28603906+VinWare@users.noreply.github.com>
2021-07-06 14:30:20 +0530

    Further modularized, new functions for more specific cases

--- logname/logname.v
+++ logname/logname.v
@@ -1,6 +1,68 @@
 import os
 import flag
 
+const (
+	version_str = 'V Coreutils 0.0.1'
+)
+
+// A default error exit, when code is not important
+fn error_exit(errors ...string) {
+	error_exit_code(1,...errors)
+}
+
+// Use only if error code is important (some semantic meaning to particular codes 
+fn error_exit_code(code int, errors ...string) {
+	for error in errors {
+		eprintln(error)
+	}
+	exit(code)
+}
+
+// Use if successful exit
+fn success_exit(messages...string) {
+	for message in messages {
+		println(message)
+	}
+	exit(0)
+}
+/*
+** Standard function to perform basic flag parsing an help and version processing
+** params: args - string array (should usually be os.args in main function)
+** returns: FlagParser object reference, exec name
+** logic: Creates a parser with given arguments. Checks if --help or --version flag are present, and prints and exits if yes
+*/
+
+fn flags_common(args []string, app_name string, free_args_min int, free_args_max int) (&flag.FlagParser, string){
+	// Flags
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application(app_name)
+	fp.limit_free_args(free_args_min,free_args_max)
+	fp.version(version_str)	// Preferably take from common version constant, should be updated
+
+	exec := fp.args[0]
+	// println(exec)
+
+	// --help and --version are standard flags for coreutils programs
+	help := fp.bool('help',0,false,'display this help and exit')
+	version := fp.bool('version',0,false,'output version information and exit')
+	
+	if help{
+		success_exit(fp.usage())
+	}
+	if version {
+		success_exit(version_str) // Needs to be modified
+	}
+
+	fp.skip_executable()
+
+	return fp, exec
+}
+
+// Use if no arguments are taken
+fn flags_common_no_args(args [] string, app_name string) (&flag.FlagParser,string) {
+	return flags_common(args,app_name,0,0)
+}
+
 /* logname
 ** VinWare, 2021-07-05 05:00:00 UTC
 **
@@ -13,54 +75,13 @@ import flag
 ** Standard way to take arguments - Solved by using flags module
 */
 
-fn error_exit(errors ...string) {
-	for error in errors {
-		eprintln(error)
-	}
-	exit(1) 	
-}
-
-/*
-** Standard function to perform basic flag parsing an help and version processing
-** params: args - string array (should usually be os.args in main function)
-** returns: FlagParser object reference - may be used if other options are there
-** logic: Creates a parser with given arguments. Checks if --help or --version flag are present, and prints and exits if yes
-*/
-
-fn get_flags_help_version(args []string, app_name string, version_str string, free_args_min int, free_args_max int) &flag.FlagParser{
-	// Flags
-	mut fp := flag.new_flag_parser(os.args)
-	fp.application(app_name)
-	fp.limit_free_args(free_args_min,free_args_max)
-	fp.version(version_str)	// Preferably take from common version constant, should be updated
-	// exec := fp.args[0]
-	// println(exec)
-
-	// --help and --version are standard flags for coreutils programs
-	help := fp.bool('help',0,false,'display this help and exit')
-	version := fp.bool('version',0,false,'output version information and exit')
-	
-	if help{
-		println(fp.usage())
-		exit(0)
-	}
-	if version {
-		println('version')
-		exit(0)
-	}
-
-	fp.skip_executable()
+fn main() {
+	// Exec is not needed
+	fp,_ := flags_common_no_args(os.args, 'logname')
 
 	fp.finalize() or {
 		error_exit(err.str(),fp.usage())
 	}
-
-	return fp
-}
-
-fn main() {
-	// No options used here, so don't store returned parser
-	get_flags_help_version(os.args, 'logname', '0.0.1', 0, 0)
 
 	// Main functionality
 
@@ -70,5 +91,6 @@ fn main() {
 		// C.getlogin failed
 		error_exit('no login name')
 	}
-	println(lname)
+	
+	success_exit(lname)
 }
