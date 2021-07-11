63498660f88fe2c09dcd45f3629847090c0b466c
Shen-Teng Tu <tusamten@gmail.com>
2021-07-11 14:21:15 +0800

    printenv: use `flag` module

--- src/printenv/printenv.v
+++ src/printenv/printenv.v
@@ -1,4 +1,5 @@
 import os
+import flag
 
 const cmd_ns = 'printenv'
 
@@ -14,22 +15,11 @@ fn unrec(arg string) string {
 }
 
 fn error_exit(error string) {
-	eprintln(error)
+	if error.len > 0 {
+		eprintln(error)
+	}
+	println("Try '$cmd_ns --help' for more information.")
 	exit(1)
-}
-
-fn print_avliable_params() {
-	mut entries := map{
-		'--help':    'display this help and exit'
-		'--version': 'output version information and exit'
-	}
-	// Other avliable parameters
-	entries['-0, --null'] = 'end each output line with NUL, not newline'
-	// Print avliable parameters
-	println('Options:')
-	for param, desp in entries {
-		println('\t${param:-16} $desp')
-	}
 }
 
 // Exit status:
@@ -37,48 +27,28 @@ fn print_avliable_params() {
 // 1 if at least one specified variable was not found
 // 2 if a write error occurred
 fn main() {
-	usage := 'Usage: $cmd_ns [OPTION]... [VARIABLE]...\n'
-	version := '$cmd_ns (V coreutils) 0.0.1'
-	args := os.args[1..]
+	// Define options
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application(cmd_ns)
+	fp.version('(V coreutils) 0.0.1')
+	fp.skip_executable()
+	fp.arguments_description('[VARIABLE]...') // 'Usage: sleep [options] arg...'
+	fp.description('Print the values of the specified environment VARIABLE(s).\n' +
+		'If no VARIABLE is specified, print name and value pairs for them all.') // 'Description: ...'
+	// Define options
+	//`-0, --null`
+	opt_nul_terminate := fp.bool('null', '0'.bytes()[0], false, 'end each output line with NUL, not newline')
 
-	mut nul_terminate := false
-	// Parameters provided
-	if args.len > 0 {
-		option := args[0]
-		match option {
-			'--help' {
-				println(usage)
-				print_avliable_params()
-				exit(0)
-			}
-			'--version' {
-				println(version)
-				exit(0)
-			}
-			'--null', '-0' {
-				nul_terminate = true
-			}
-			else {
-				if option[0..1] == '-' {
-					error_exit(unrec(option))
-				}
-			}
-		}
+	args := fp.finalize() or {
+		error_exit(err.msg)
+		exit(1)
 	}
 
 	// Main functionality
-	mut vars := []string{}
-	if nul_terminate {
-		vars << args[1..]
-	} else {
-		vars << args
-	}
-	if vars.len == 0 {
-		// TODO : resolve the issue about
-		// different output order from the original printenv
+	if args.len == 0 {
 		for k, v in os.environ() {
 			mut s := '$k=$v'
-			if nul_terminate {
+			if opt_nul_terminate {
 				print(s)
 				print(zero_byte)
 			} else {
@@ -87,13 +57,13 @@ fn main() {
 		}
 	} else {
 		mut code := 0 // exit code
-		for k in vars {
+		for k in args {
 			mut v := os.getenv(k)
 			if v == '' {
 				code = 1 // at least one specified variable was not found
 				continue
 			}
-			if nul_terminate {
+			if opt_nul_terminate {
 				print(v)
 				print(zero_byte)
 			} else {
