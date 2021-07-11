e8083c267963b82d952e1ed66493632af8c56ac9
Delyan Angelov <delian66@gmail.com>
2021-07-11 18:38:24 +0300

    whoami: cleanup, add test

--- src/whoami/whoami.v
+++ src/whoami/whoami.v
@@ -1,50 +1,26 @@
 import os
+import common
 
-fn try(arg string) string {
-	return 'whoami: unknown argument: $arg\nUse whoami --help to see options'
+#include <pwd.h>
+
+struct C.passwd {
+	pw_name &char
 }
 
-fn unrec(arg string) string {
-	return 'whoami: unrecognized option $arg\nUse whoami --help to see options'
-}
-
-fn error_exit(error string) {
-	eprintln(error)
-	exit(1)
-}
+fn C.getpwuid(uid int) &C.passwd
 
 fn main() {
-	usage := 'Usage: whoami [OPTION]. [OPTION] can be --help, --version'
-	version := 'whoami (V coreutils) 0.0.1'
-	args := os.args[1..]
-	params := args.filter(it.len > 2 && it[0..2] == '--')
-	if params.len > 0 {
-		// Parameters provided
-		match params[0] {
-			'--help' {
-				println(usage)
-				exit(0)
-			}
-			'--version' {
-				println(version)
-				exit(0)
-			}
-			else {
-				error_exit(unrec(params[0]))
-			}
-		}
-		exit(0)
+	mut fp := common.flag_parser(os.args)
+	fp.application('whoami')
+	fp.description('Print the user name associated with the current effective user ID.')
+	fp.description('Same as id -un.')
+	fp.limit_free_args_to_exactly(0)
+	fp.remaining_parameters()
+	uid := os.geteuid()
+	if uid == -1 {
+		common.exit_with_error_message('whoami', 'no user name')
 	}
-	if args.len > 0 {
-		// Unnecessary argument
-		error_exit(try(args[0]))
-	}
-	// Main functionality
-	// Internally uses C.getlogin
-	user := os.loginname()
-	if user == '' {
-		// C.getlogin failed
-		error_exit('no user name')
-	}
-	println(user)
+	pwd := C.getpwuid(uid)
+	username := unsafe { cstring_to_vstring(pwd.pw_name) }
+	println(username)
 }
