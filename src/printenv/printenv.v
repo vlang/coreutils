4ac6c16c830612730195b5b7eef9b3624138d4e9
Delyan Angelov <delian66@gmail.com>
2021-07-11 15:45:34 +0300

    use `import common` for the version

--- src/printenv/printenv.v
+++ src/printenv/printenv.v
@@ -1,42 +1,21 @@
 import os
 import flag
-
-const cmd_ns = 'printenv'
+import common
 
 const zero_byte = byte(0).ascii_str()
-
-/*
-fn try(arg string) string {
-	return '$cmd_ns: unknown argument: $arg\nUse $cmd_ns --help to see options'
-}*/
-
-fn unrec(arg string) string {
-	return '$cmd_ns: unrecognized option $arg\nUse $cmd_ns --help to see options'
-}
-
-fn error_exit(error string) {
-	if error.len > 0 {
-		eprintln(error)
-	}
-	println("Try '$cmd_ns --help' for more information.")
-	exit(1)
-}
 
 // Exit status:
 // 0 if all variables specified were found
 // 1 if at least one specified variable was not found
 // 2 if a write error occurred
 fn main() {
-	// Define options
-	mut fp := flag.new_flag_parser(os.args)
-	fp.application(cmd_ns)
-	fp.version('(V coreutils) 0.0.1')
-	fp.skip_executable()
+	mut exit_code := 0
+	mut fp := common.flag_parser(os.args)
+	fp.application('printenv')
 	fp.arguments_description('[VARIABLE]...')
 	fp.description('Print the values of the specified environment VARIABLE(s).')
 	fp.description('If no VARIABLE is specified, print name and value pairs for them all.')
-	mut exit_code := 0
-	mut opt_nul_terminate := fp.bool('null', '0'[0], false, 'end each output line with NUL, not newline')
+	mut opt_nul_terminate := fp.bool('null', `0`, false, 'end each output line with NUL, not newline')
 	if opt_nul_terminate {
 		if os.args[1] !in ['-0', '--null'] {
 			// GNU printenv has a quirk,
