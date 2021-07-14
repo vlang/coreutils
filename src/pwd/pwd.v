7c9f64dd1f92ce0dfc52626b7cec67c1ead07fc1
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-14 08:40:05 -0900

    add args

--- src/pwd/pwd.v
+++ src/pwd/pwd.v
@@ -1,6 +1,19 @@
 import os
 
 fn main() {
+	args = os.args
 	path := os.getwd()
-	println(path)
+	if os.user_os() == "windows" {
+		if args[1] == "-L" {
+			os.execute("echo %cd%")
+		} else if args[1] == "-P" {
+			println(path)
+		}
+	} else {
+		if args[1] == "-L" {
+			os.execute("echo $PWD")
+		} else if args[1] == "-P" {
+			println(path)
+		}
+	}
 }
