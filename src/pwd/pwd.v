7803c278c1977f81fe8f68c844012612fca169c4
Cooper <c112097@yahoo.com>
2021-07-14 10:42:30 -0700

    fmt

--- src/pwd/pwd.v
+++ src/pwd/pwd.v
@@ -3,16 +3,16 @@ import os
 fn main() {
 	args = os.args
 	path := os.getwd()
-	if os.user_os() == "windows" {
-		if args[1] == "-L" {
-			os.execute("echo %cd%")
-		} else if args[1] == "-P" {
+	if os.user_os() == 'windows' {
+		if args[1] == '-L' {
+			os.execute('echo %cd%')
+		} else if args[1] == '-P' {
 			println(path)
 		}
 	} else {
-		if args[1] == "-L" {
-			os.execute("echo $PWD")
-		} else if args[1] == "-P" {
+		if args[1] == '-L' {
+			os.execute('echo $PWD')
+		} else if args[1] == '-P' {
 			println(path)
 		}
 	}
