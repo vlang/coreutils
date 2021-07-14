1d918f5743f846abdc745c7ecaacccb69ddfa44c
VinWare <28603906+VinWare@users.noreply.github.com>
2021-07-14 13:29:09 +0530

    Main flags implemented

--- src/mv/MvCommand.v
+++ src/mv/MvCommand.v
@@ -1,4 +1,11 @@
 struct MvCommand {
+	force               bool
+	interactive         bool
+	no_clobber          bool
+	update              bool
+	verbose             bool
+	target_directory    string
+	no_target_directory bool
 }
 
 fn (m MvCommand) run() {
--- src/mv/helper.v
+++ src/mv/helper.v
@@ -5,14 +5,24 @@ const (
 )
 
 pub fn run_mv(args []string) {
-	mv := setup_mv_command(args) or { common.exit_with_error_message(name, err.msg) }
+	mv, sources, dest := setup_mv_command(args) or { common.exit_with_error_message(name, err.msg) }
+	println(sources)
+	println(dest)
 	mv.run()
 }
 
-fn setup_mv_command(args []string) ?MvCommand {
+fn setup_mv_command(args []string) ?(MvCommand, []string, string) {
 	mut fp := common.flag_parser(args)
 	fp.application('mv')
-	fp.limit_free_args_to_at_least(2)
+	fp.limit_free_args_to_at_least(1)
+
+	force := fp.bool('force', `f`, false, 'force')
+	interactive := fp.bool('interactive', `i`, false, 'interactive')
+	no_clobber := fp.bool('no-clobber', `n`, false, 'no-clobber')
+	update := fp.bool('update', `u`, false, 'update')
+	verbose := fp.bool('verbose', `v`, false, 'verbose')
+	target_directory := fp.string('target-directory', `t`, '', 'target-directory')
+	no_target_directory := fp.bool('no-target-directory', `T`, false, 'no-target-directory')
 
 	help := fp.bool('help', 0, false, 'display this help and exit')
 	version := fp.bool('version', 0, false, 'output version information and exit')
@@ -22,7 +32,27 @@ fn setup_mv_command(args []string) ?MvCommand {
 	if version {
 		success_exit('rm $common.coreutils_version()')
 	}
-	return MvCommand{}
+
+	options := fp.finalize() or { common.exit_with_error_message(name, 'error') }
+	len_options := options.len
+	if target_directory != '' && len_options < 2 {
+		common.exit_with_error_message(name, 'error')
+	}
+	sources, dest := if target_directory != '' {
+		options, target_directory
+	} else {
+		options[0..len_options - 1], options[len_options - 1]
+	}
+
+	return MvCommand{
+		force: force
+		interactive: interactive
+		no_clobber: no_clobber
+		update: update
+		verbose: verbose
+		target_directory: target_directory
+		no_target_directory: no_target_directory
+	}, sources, dest
 }
 
 // Print messages and exit
