344418f18be5cc9e70b4f08e738d83ac6ba59d09
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-07-14 08:51:53 -0900

    Update pwd.v

--- src/pwd/pwd.v
+++ src/pwd/pwd.v
@@ -1,19 +1,18 @@
 import os
 
 fn main() {
-	args := os.args
 	path := os.getwd()
 	if os.user_os() == 'windows' {
-		if args[1] == '-L' {
+		if os.args[1] == '-L' {
 			os.execute('echo %cd%')
-		} else if args[1] == '-P' {
+		} else if os.args[1] == '-P' {
 			println(path)
 		}
 	} else {
-		if args[1] == '-L' {
+		if os.args[1] == '-L' {
 			PWD := os.getenv('PWD')
 			os.execute('echo $PWD')
-		} else if args[1] == '-P' {
+		} else if os.args[1] == '-P' {
 			println(path)
 		}
 	}
