a7456ef6b5b2ece840da3a225017ca637067309b
lemoncmd <lemoncmd@gmail.com>
2021-08-03 01:04:37 +0900

    common: add comment for is_utf8

--- common/common.c.v
+++ common/common.c.v
@@ -8,6 +8,7 @@ fn init() {
 	C.setlocale(C.LC_ALL, ''.str)
 }
 
+// is_utf8 returns whether the locale supports UTF-8 or not
 pub fn is_utf8() bool {
 	locale := unsafe { C.setlocale(C.LC_CTYPE, &char(0)).vstring().to_lower() }
 	return locale.contains_any_substr(['utf8', 'utf-8'])
