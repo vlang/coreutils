59d0d56a3bd5ce4949ecf83465bb118c33902aea
Taillook <jousoku@gmail.com>
2021-08-29 23:04:05 +0900

    use common module `true` (#65)

--- src/true/true.v
+++ src/true/true.v
@@ -1,20 +1,17 @@
 module main
 
-import flag
+import common
 import os
 
 const (
-	app_name    = 'true'
-	app_version = 'v0.0.1'
+	app_name = 'true'
 )
 
 fn true_fn() {
-	mut fp := flag.new_flag_parser(os.args)
+	mut fp := common.flag_parser(os.args)
 	fp.application(app_name)
-	fp.version(app_version)
 	fp.description('Exit with a status code indicating success.')
 	fp.limit_free_args(0, 0)
-	fp.skip_executable()
 	fp.finalize() or {}
 
 	exit(0)
