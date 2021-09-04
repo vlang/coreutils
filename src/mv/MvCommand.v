572c27cbd793cbae7bbfc074b8f1634872213fa4
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-04 09:24:06 +0530

    Working implementation of `mv` (#68)
    * Basic rm
    
    * Some basic checks added
    
    * Program compiles, recursive not working
    
    * Use os method for recursive
    
    * vfmt run
    
    * Restructured
    
    * Completed basic logic. All in one file currently
    
    * Basic logic done. Using common module now
    
    * Format
    
    * Modularized the code
    
    * Vfmt run
    
    * Added some tests, cleaned up some code
    
    * Added check for exists earlier
    
    * vfmt
    
    * Checked in README
    
    * Start mv implementation
    
    * v fmt
    
    * Formatting again, for some reason check failed
    
    * Make program compile with -prod
    
    * Reorganized to avoid nesting
    
    * vfmt run
    
    * Temporarily turned off tests
    
    * Removed test file since for some reason it is still running
    
    * Mistakenly removed helper file instead of test file
    
    * Removed test file
    
    * Added tests
    
    * v fmt run
    
    * Delete README
    
    * Rearranged mv
    
    * Main flags implemented
    
    * Basic logic ready, flags and edge cases remain
    
    * Added prompt funcs, definition of move
    
    * Format

---
+++ src/mv/MvCommand.v
@@ -0,0 +1,61 @@
+import os
+// import common
+
+enum OverwriteMode {
+	force
+	interactive
+	no_clobber
+}
+
+struct MvCommand {
+	overwrite OverwriteMode
+	// force               bool
+	// interactive         bool
+	// no_clobber          bool
+	update              bool
+	verbose             bool
+	target_directory    string
+	no_target_directory bool
+}
+
+fn (m MvCommand) run(source string, dest string) {
+	if !os.exists(source) {
+		eprintln(not_exist(source))
+		return
+	}
+	if m.verbose || m.overwrite != .force {
+		m.move(source, dest)
+		// m.mv(source,dest)
+	}
+	os.mv(source, dest) or { error_exit(name, err.msg) }
+	// if os.is_dir()
+	// println(name)
+}
+
+fn (m MvCommand) move(src string, dst string) {
+	ndst := if os.is_dir(dst) {
+		os.join_path(dst.trim_right(os.path_separator), os.file_name(src.trim_right(os.path_separator)))
+	} else {
+		dst
+	}
+
+	rdst := $if windows { ndst.replace('/', '\\') } $else { ndst }
+	if !m.int_yes(rdst) {
+		return
+	}
+	os.mv(src, rdst) or { return }
+	if m.verbose {
+		println(renamed(src, dst))
+	}
+}
+
+fn (m MvCommand) int_yes(path string) bool {
+	if os.exists(path) {
+		match m.overwrite {
+			.no_clobber { return false }
+			.interactive { return int_yes(prompt_file(path)) }
+			.force { return true }
+		}
+	}
+	return true
+}
---
+++ src/mv/helper.v
@@ -0,0 +1,152 @@
+import os
+import common
+
+const (
+	name            = 'mv'
+	interactive_yes = ['y']
+	combine_t_no_t  = 'cannot combine --target-directory (-t) and --no-target-directory (-T)'
+)
+
+fn prompt_file(path string) string {
+	return "overwrite '$path'? "
+}
+
+fn target_not_dir(path string) string {
+	return "target '$path' is not a directory"
+}
+
+fn renamed(src string, dst string) string {
+	return "renamed '$src' -> '$dst'"
+}
+
+fn missing_dest(path string) string {
+	return "missing destination file operand after '$path'"
+}
+
+fn no_dir_is_dir(path string) string {
+	return "cannot overwrite directory '$path' with non-directory"
+}
+
+fn not_exist(path string) string {
+	return "$name: cannot stat '$path': No such file or directory"
+}
+
+fn extra_operand(operand string) string {
+	return "extra operand '$operand'"
+}
+
+fn valid_yes(input string) bool {
+	mut is_yes := false
+	low_input := input.to_lower()
+	for yes in interactive_yes {
+		is_yes = is_yes || low_input.starts_with(yes)
+	}
+	return is_yes
+}
+
+// Take user confirmation and check if it is considered yes
+fn int_yes(prompt string) bool {
+	return valid_yes(os.input(prompt))
+}
+
+pub fn run_mv(args []string) {
+	mv, sources, dest := setup_mv_command(args) or { common.exit_with_error_message(name, err.msg) }
+	// print(sources)
+	if sources.len > 1 && !os.is_dir(dest) {
+		common.exit_with_error_message(name, target_not_dir(dest))
+	}
+	for source in sources {
+		// println(source)
+		mv.run(source, dest)
+	}
+	// mv.run(sources,dest)
+}
+
+fn setup_mv_command(args []string) ?(MvCommand, []string, string) {
+	mut fp := common.flag_parser(args)
+	fp.application('mv')
+	fp.limit_free_args_to_at_least(1)
+
+	force := fp.bool('force', `f`, false, 'ignore interactive and no-clobber')
+	interactive := fp.bool('interactive', `i`, false, 'ask for each overwrite')
+	no_clobber := fp.bool('no-clobber', `n`, false, 'do not overwrite')
+	update := fp.bool('update', `u`, false, 'update')
+	verbose := fp.bool('verbose', `v`, false, 'print each rename')
+	target_directory := fp.string('target-directory', `t`, '', 'target-directory')
+	no_target_directory := fp.bool('no-target-directory', `T`, false, 'no-target-directory')
+
+	help := fp.bool('help', 0, false, 'display this help and exit')
+	version := fp.bool('version', 0, false, 'output version information and exit')
+	if help {
+		success_exit(fp.usage())
+	}
+	if version {
+		success_exit('rm $common.coreutils_version()')
+	}
+
+	options := fp.finalize() or { common.exit_with_error_message(name, 'error') }
+	overwrite := if force {
+		OverwriteMode.force
+	} else if no_clobber {
+		OverwriteMode.no_clobber
+	} else if interactive {
+		OverwriteMode.interactive
+	} else {
+		OverwriteMode.force
+	}
+	len_options := options.len
+	if target_directory != '' {
+		if no_target_directory {
+			common.exit_with_error_message(name, combine_t_no_t)
+		}
+		if !os.exists(target_directory) {
+			common.exit_with_error_message(name, not_exist(target_directory))
+		}
+		if !os.is_dir(target_directory) {
+			common.exit_with_error_message(name, target_not_dir(target_directory))
+		}
+	} else {
+		if len_options < 2 {
+			common.exit_with_error_message(name, missing_dest(options[0]))
+		}
+		if no_target_directory {
+			if len_options > 2 {
+				common.exit_with_error_message(name, extra_operand(options[2]))
+			}
+			if os.is_dir(options[1]) {
+				common.exit_with_error_message(name, no_dir_is_dir(options[1]))
+			}
+		}
+	}
+	sources, dest := if target_directory != '' {
+		options, target_directory
+	} else {
+		options[0..len_options - 1], options[len_options - 1]
+	}
+
+	return MvCommand{
+		overwrite: overwrite
+		update: update
+		verbose: verbose
+		target_directory: target_directory
+		no_target_directory: no_target_directory
+	}, sources, dest
+}
+
+// Print messages and exit with error
+[noreturn]
+fn error_exit(messages ...string) {
+	for message in messages {
+		eprintln(message)
+	}
+	exit(1)
+}
+
+// Print messages and exit
+[noreturn]
+fn success_exit(messages ...string) {
+	for message in messages {
+		println(message)
+	}
+	exit(0)
+}
