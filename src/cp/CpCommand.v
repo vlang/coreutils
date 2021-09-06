f9a1e1b39c79d95faeb4acccc8fa4188dbc2446b
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-06 22:01:51 +0530

    Basic working of cp

--- src/cp/CpCommand.v
+++ src/cp/CpCommand.v
@@ -1,2 +1,60 @@
+import os
+
+enum OverwriteMode {
+	force
+	interactive
+	no_clobber
+}
+
 struct CpCommand {
+	overwrite           OverwriteMode
+	update              bool
+	verbose             bool
+	target_directory    string
+	no_target_directory bool
+	recursive           bool
+}
+
+fn (c CpCommand) run(source string, dest string) {
+	if !os.exists(source) {
+		eprintln(not_exist(source))
+		return
+	}
+	if c.verbose || c.overwrite != .force {
+		c.copy(source, dest)
+	} else {
+		if os.is_dir(source) && !c.recursive {
+			eprintln(not_recursive(source))
+			return
+		}
+		os.cp(source, dest) or { error_exit(name, err.msg) }
+	}
+}
+
+fn (c CpCommand) copy(src string, dst string) {
+	ndst := if os.is_dir(dst) {
+		os.join_path(dst.trim_right(os.path_separator), os.file_name(src.trim_right(os.path_separator)))
+	} else {
+		dst
+	}
+
+	rdst := $if windows { ndst.replace('/', '\\') } $else { ndst }
+	if !c.int_yes(rdst) {
+		return
+	}
+	os.cp(src, rdst) or { return }
+	if c.verbose {
+		println(renamed(src, dst))
+	}
+}
+
+fn (c CpCommand) int_yes(path string) bool {
+	if os.exists(path) {
+		match c.overwrite {
+			.no_clobber { return false }
+			.interactive { return int_yes(prompt_file(path)) }
+			.force { return true }
+		}
+	}
+	return true
 }
--- src/cp/helper.v
+++ src/cp/helper.v
@@ -1,9 +1,58 @@
+import os
 import common
 
 const (
-	name = 'cp'
+	name            = 'cp'
+	interactive_yes = ['y']
+	combine_t_no_t  = 'cannot combine --target-directory (-t) and --no-target-directory (-T)'
 )
- 
+
+fn prompt_file(path string) string {
+	return "overwrite '$path'? "
+}
+
+fn target_not_dir(path string) string {
+	return "target '$path' is not a directory"
+}
+
+fn renamed(src string, dst string) string {
+	return "renamed '$src' -> '$dst'"
+}
+
+fn missing_dest(path string) string {
+	return "missing destination file operand after '$path'"
+}
+
+fn no_dir_is_dir(path string) string {
+	return "cannot overwrite directory '$path' with non-directory"
+}
+
+fn not_exist(path string) string {
+	return "$name: cannot stat '$path': No such file or directory"
+}
+
+fn extra_operand(operand string) string {
+	return "extra operand '$operand'"
+}
+
+fn valid_yes(input string) bool {
+	mut is_yes := false
+	low_input := input.to_lower()
+	for yes in interactive_yes {
+		is_yes = is_yes || low_input.starts_with(yes)
+	}
+	return is_yes
+}
+
+// Take user confirmation and check if it is considered yes
+fn int_yes(prompt string) bool {
+	return valid_yes(os.input(prompt))
+}
+
+fn not_recursive(path string) string {
+	return "$name: -r not specified; omitting directory '$path'"
+}
+
 // Print messages and exit with error
 [noreturn]
 fn error_exit(messages ...string) {
@@ -27,6 +76,15 @@ fn setup_cp_command(args []string) ?(CpCommand, []string, string) {
 	fp.application('cp')
 	fp.limit_free_args_to_at_least(1)
 
+	force := fp.bool('force', `f`, false, 'ignore interactive and no-clobber')
+	interactive := fp.bool('interactive', `i`, false, 'ask for each overwrite')
+	no_clobber := fp.bool('no-clobber', `n`, false, 'do not overwrite')
+	update := fp.bool('update', `u`, false, 'update')
+	verbose := fp.bool('verbose', `v`, false, 'print each rename')
+	target_directory := fp.string('target-directory', `t`, '', 'target-directory')
+	no_target_directory := fp.bool('no-target-directory', `T`, false, 'no-target-directory')
+	recursive := fp.bool('recursive', `r`, false, 'recursive')
+
 	help := fp.bool('help', 0, false, 'display this help and exit')
 	version := fp.bool('version', 0, false, 'output version information and exit')
 	if help {
@@ -36,12 +94,62 @@ fn setup_cp_command(args []string) ?(CpCommand, []string, string) {
 		success_exit('$name $common.coreutils_version()')
 	}
 
-	sources := []string{}
-	dest := ''
-	return CpCommand{}, sources, dest
+	options := fp.finalize() or { common.exit_with_error_message(name, 'error') }
+	overwrite := if force {
+		OverwriteMode.force
+	} else if no_clobber {
+		OverwriteMode.no_clobber
+	} else if interactive {
+		OverwriteMode.interactive
+	} else {
+		OverwriteMode.force
+	}
+	len_options := options.len
+	if target_directory != '' {
+		if no_target_directory {
+			common.exit_with_error_message(name, combine_t_no_t)
+		}
+		if !os.exists(target_directory) {
+			common.exit_with_error_message(name, not_exist(target_directory))
+		}
+		if !os.is_dir(target_directory) {
+			common.exit_with_error_message(name, target_not_dir(target_directory))
+		}
+	} else {
+		if len_options < 2 {
+			common.exit_with_error_message(name, missing_dest(options[0]))
+		}
+		if no_target_directory {
+			if len_options > 2 {
+				common.exit_with_error_message(name, extra_operand(options[2]))
+			}
+			if os.is_dir(options[1]) {
+				common.exit_with_error_message(name, no_dir_is_dir(options[1]))
+			}
+		}
+	}
+	sources, dest := if target_directory != '' {
+		options, target_directory
+	} else {
+		options[0..len_options - 1], options[len_options - 1]
+	}
+
+	return CpCommand{
+		overwrite: overwrite
+		update: update
+		verbose: verbose
+		target_directory: target_directory
+		no_target_directory: no_target_directory
+		recursive: recursive
+	}, sources, dest
 }
 
 fn run_cp(args []string) {
 	cp, sources, dest := setup_cp_command(args) or { common.exit_with_error_message(name, err.msg) }
-	// println('running cp')
+	if sources.len > 1 && !os.is_dir(dest) {
+		common.exit_with_error_message(name, target_not_dir(dest))
+	}
+	for source in sources {
+		cp.run(source, dest)
+	}
 }
