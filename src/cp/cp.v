99f65db92a4ed6150b73d7d1f08ecd950e1aee50
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-11 18:36:35 +0530

    Format

--- src/cp/cp.v
+++ src/cp/cp.v
@@ -1,13 +1,5 @@
-module main
-
 import os
-import src.cp
 
 fn main() {
-<<<<<<< HEAD
 	run_cp(os.args)
-	// 
-=======
-	cp.run_cp(os.args)
->>>>>>> e94acd3 (Rearranged modules to make the main executable of module main)
 }
