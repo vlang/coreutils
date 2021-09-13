3d9a2c8b7e4ac7748f0194e978e52bfa765edfff
VinWare <28603906+VinWare@users.noreply.github.com>
2021-09-13 23:15:44 +0530

    Base 32 both encoding and decoding implemented

--- src/base32/helper.v
+++ src/base32/helper.v
@@ -4,33 +4,72 @@ import strings
 import common
 
 const (
-	name         = 'base32'
-	block_size   = 5
-	group_size   = 5
-	bits_in_byte = 8
-	char_set     = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`,
-		`P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`, `2`, `3`, `4`, `5`, `6`, `7`, `=`]
+	name              = 'base32'
+	block_size        = 5
+	group_size        = 5
+	bits_in_byte      = 8
+	init_builder_size = 1024
+	end_byte          = `=`
+	char_set          = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`,
+		`O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`, `2`, `3`, `4`, `5`, `6`, `7`,
+		`=`,
+	]
+	invalid           = 'invalid input'
 )
 
+fn get_blocks(groups []byte) []byte {
+	mut full_block := bitfield.new(block_size * bits_in_byte)
+	mut i := 0
+	for group in groups {
+		byte_val := if `A` <= group && group <= `Z` {
+			byte(group - `A`)
+		} else if `2` <= group && group <= `7` {
+			byte(26 + group - `2`)
+		} else {
+			byte(1 << group_size)
+		}
+		full_block.insert(i, group_size, byte_val)
+		i += group_size
+	}
+	size := full_block.get_size()
+	mut res := []byte{}
+	for i = 0; i < size; i += bits_in_byte {
+		res << byte(full_block.extract(i, bits_in_byte))
+	}
+	return res
+}
+
 fn decode_and_output(file os.File, wrap int) {
+	mut groups := []byte{len: block_size * bits_in_byte / group_size}
+	mut builder := strings.new_builder(init_builder_size)
+	mut i := u64(0)
+	for {
+		num := file.read_bytes_into(i, mut groups) or {
+			common.exit_with_error_message(name, err.msg)
+		}
+		if num == 0 || (num == 1 && groups[0] == `\n`) {
+			break
+		} else if num < groups.len {
+			common.exit_with_error_message(name, invalid)
+		}
+		i += u64(num)
+		builder.write(get_blocks(groups)) or { common.exit_with_error_message(name, err.msg) }
+	}
+	print(builder.str())
 }
 
 // IMP: This code assumes that each group_size <= 8 bits (1 byte).
 // If using groups without this property, change the return type
 fn get_groups(block []byte, num byte) []byte {
-	// bits := (byte(1)<<bits_size) - 1
 	full_block := bitfield.from_bytes(block)
 	mut groups := []byte{}
 	num_blocks := block_size * bits_in_byte / group_size
-	// for i := num_blocks - 1 ; i >= 0; i-- {
-	// shift := groups_size * i
-	// groups << group
-	// }
 	for i in 0 .. num_blocks {
-		groups << byte(full_block.extract(i * group_size, group_size))
+		byte_val := byte(full_block.extract(i * group_size, group_size))
+		groups << if byte_val < 26 { byte_val + `A` } else { byte_val - 26 + `2` }
 	}
 	for i in 0 .. num {
-		groups[groups.len - i - 1] = byte(1) << group_size
+		groups[groups.len - i - 1] = end_byte
 	}
 	return groups
 }
@@ -41,8 +80,14 @@ fn encode_and_output(file os.File, wrap int) {
 	mut done := false
 	for !done {
 		num := file.read_bytes_into(i, mut block) or {
-			// println('here')
-			common.exit_with_error_message(name, err.msg)
+			match err {
+				none {
+					0
+				}
+				else {
+					common.exit_with_error_message(name, err.msg)
+				}
+			}
 		}
 		if num == 0 {
 			break
@@ -63,11 +108,8 @@ fn encode_and_output(file os.File, wrap int) {
 		}
 		groups := get_groups(block, byte(num_equal))
 		mut result := strings.new_builder(groups.len)
-		for group in groups {
-			result.write_b(char_set[group])
-		}
+		result.write(groups) or { common.exit_with_error_message(name, err.msg) }
 		print(result.str())
-		// println(groups)
 	}
 	println('')
 }
