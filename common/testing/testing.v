79b062ac99876e786694560cae6aae80e79bf7ec
Delyan Angelov <delian66@gmail.com>
2021-12-01 15:04:51 +0200

    ci: fix the uptime test failing for non-gnu-coreutils uptime without GNU_COREUTILS_INSTALLED=1

--- common/testing/testing.v
+++ common/testing/testing.v
@@ -114,8 +114,10 @@ pub fn same_results(cmd1 string, cmd2 string) bool {
 	if cmd1.contains('printenv') && cmd2.contains('printenv.exe') {
 		return cmd1_res.exit_code == cmd2_res.exit_code
 	}
-	if cmd1 == 'uptime /var/log/wtmp' && cmd2.contains('uptime.exe /var/log/wtmp') {
+	if (cmd1 == 'uptime /var/log/wtmp' && cmd2.ends_with('uptime.exe /var/log/wtmp'))
+		|| (cmd1 == 'uptime' && cmd2.ends_with('uptime.exe')) {
 		return cmd1_res.exit_code == cmd2_res.exit_code
+			&& cmd1_res.output.all_after('load average:') == cmd2_res.output.all_after('load average:')
 	}
 	// in all other cases, compare the normalised output (less strict):
 	return cmd1_res.exit_code == cmd2_res.exit_code && noutput1 == noutput2
