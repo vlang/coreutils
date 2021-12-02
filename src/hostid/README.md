0f90b16c58777a62fa79f2ef1be38031a1e8dc6d
Benoit Malhomme <sheatnoisette@protonmail.com>
2021-12-02 11:10:26 +0100

    hostid: Initial implementation

---
+++ src/hostid/README.md
@@ -0,0 +1,5 @@
+# hostid
+Implementation of hostid as in GNU Coreutils in V
+
+Author:
+B "SheatNoisette" Malhomme
---
+++ src/hostid/gethostid_default.c.v
@@ -0,0 +1,8 @@
+
+/*
+** Get hostid on unsupported platforms
+*/
+fn hd_get_hostid() u32 {
+	eprintln("Unsupported platform")
+	return 0
+}
