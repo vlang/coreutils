01ebe77eed8f7d3888c448d71f3fc165244cf2a4
Delyan Angelov <delian66@gmail.com>
2022-10-16 16:56:42 +0300

    cleanup wc_test.v

--- src/wc/wc_test.v
+++ src/wc/wc_test.v
@@ -9,6 +9,16 @@ fn test_help_and_version() {
 	cmd.ensure_help_and_version_options_work()!
 }
 
+const test_txt_path = os.join_path(testing.temp_folder, 'test.txt')
+
+fn testsuite_begin() {
+	os.write_file(test_txt_path, 'Hello World!\nHow are you?')!
+}
+
+fn testsuite_end() {
+	os.rm(test_txt_path)!
+}
+
 fn test_abcd() {
 	res := os.execute('$the_executable abcd')
 	assert res.exit_code == 1
@@ -16,40 +26,19 @@ fn test_abcd() {
 }
 
 fn test_default() {
-	mut f := os.open_file('textfile', 'w') or { panic(err) }
-	f.write_string('Hello World!\nHow are you?') or {}
-	f.close()
-	defer {
-		os.rm('textfile') or { panic(err) }
-	}
-
-	res := os.execute('$the_executable textfile')
+	res := os.execute('$the_executable $test_txt_path')
 	assert res.exit_code == 0
-	assert res.output == ' 1  5 25 textfile\n'
+	assert res.output == ' 1  5 25 $test_txt_path\n'
 }
 
 fn test_max_line_length() {
-	mut f := os.open_file('textfile', 'w') or { panic(err) }
-	f.write_string('Hello World!\nHow are you?') or {}
-	f.close()
-	defer {
-		os.rm('textfile') or { panic(err) }
-	}
-
-	res := os.execute('$the_executable -L textfile')
+	res := os.execute('$the_executable -L $test_txt_path')
 	assert res.exit_code == 0
-	assert res.output == '12 textfile\n'
+	assert res.output == '12 $test_txt_path\n'
 }
 
 fn test_char_count() {
-	mut f := os.open_file('textfile', 'w') or { panic(err) }
-	f.write_string('Hello World!\nHow are you?') or {}
-	f.close()
-	defer {
-		os.rm('textfile') or { panic(err) }
-	}
-
-	res := os.execute('$the_executable -m textfile')
+	res := os.execute('$the_executable -m $test_txt_path')
 	assert res.exit_code == 0
-	assert res.output == '25 textfile\n'
+	assert res.output == '25 $test_txt_path\n'
 }
