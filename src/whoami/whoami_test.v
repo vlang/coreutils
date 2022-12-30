18c0ff01b80965be85e739e1bfbd29cc0e16bb60
Roy Ivy III <rivy.dev@gmail.com>
2022-12-29 22:30:59 -0600

    test/whoami: disable testing for WinOS

--- src/whoami/whoami_test.v
+++ src/whoami/whoami_test.v
@@ -1,17 +1,38 @@
 import common.testing
 
-const the_executable = testing.prepare_executable('whoami')
+const util = 'whoami'
 
-const cmd = testing.new_paired_command('whoami', the_executable)
+const supported_platform = $if windows {
+	false
+} $else {
+	true
+} // WinOS lacks currently required utmp support
+
+const the_executable = if supported_platform {
+	testing.prepare_executable(util)
+} else {
+	''
+}
+
+const cmd = testing.new_paired_command(util, the_executable)
 
 fn test_help_and_version() {
+	if !supported_platform {
+		return
+	}
 	cmd.ensure_help_and_version_options_work()!
 }
 
 fn test_unknown_option() {
+	if !supported_platform {
+		return
+	}
 	testing.command_fails('${the_executable} -x')!
 }
 
 fn test_print_machine_arch() {
+	if !supported_platform {
+		return
+	}
 	assert cmd.same_results('')
 }
