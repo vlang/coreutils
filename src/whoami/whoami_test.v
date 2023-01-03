9711ea9dd921d82203ab548f131d376fb0184d8f
Roy Ivy III <rivy.dev@gmail.com>
2023-01-03 11:23:11 -0600

    test/whoami ~ test vs `coreutils` on WinOS

--- src/whoami/whoami_test.v
+++ src/whoami/whoami_test.v
@@ -1,38 +1,20 @@
 import common.testing
 
 const util = 'whoami'
+const platform_util = $if !windows { util } $else { "coreutils ${util}" }
 
-const supported_platform = $if windows {
-	false
-} $else {
-	true
-} // WinOS lacks currently required utmp support
+const the_executable = testing.prepare_executable(util)
 
-const the_executable = if supported_platform {
-	testing.prepare_executable(util)
-} else {
-	''
-}
-
-const cmd = testing.new_paired_command(util, the_executable)
+const cmd = testing.new_paired_command(platform_util, the_executable)
 
 fn test_help_and_version() {
-	if !supported_platform {
-		return
-	}
 	cmd.ensure_help_and_version_options_work()!
 }
 
 fn test_unknown_option() {
-	if !supported_platform {
-		return
-	}
 	testing.command_fails('${the_executable} -x')!
 }
 
-fn test_print_machine_arch() {
-	if !supported_platform {
-		return
-	}
+fn test_display_username() {
 	assert cmd.same_results('')
 }
