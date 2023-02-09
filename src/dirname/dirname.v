ede33a6e04bb0010819f171a7c934e3076657ef0
Morgan Peterson <lastyearsmodel@gmail.com>
2023-02-09 16:31:08 -0500

    Updated code using os.dir

--- src/dirname/dirname.v
+++ src/dirname/dirname.v
@@ -5,22 +5,34 @@ import common
 
 fn main() {
 	mut fp := common.flag_parser(os.args)
+
 	fp.application('dirname')
-	fp.description('strip non-directory suffix from file name')
+	fp.description('strip the last file name component')
 
 	is_zero := fp.bool('zero', `z`, false, '')
+
 	fp.allow_unknown_args()
+
 	args := fp.remaining_parameters()
+
+	exit_success := 0
 
 	// Empty args == '.'
 	if args.len == 0 {
 		print_out('.', is_zero)
+		exit(exit_success)
 	}
 
 	for arg in args {
 		d := dirname(arg)
 		print_out(d, is_zero)
 	}
+	// EXIT_SUCCESS
+	exit(exit_success)
+}
+
+fn is_slash(c u8) bool {
+	return c == u8(47)
 }
 
 fn print_out(out string, is_zero bool) {
@@ -31,36 +43,31 @@ fn print_out(out string, is_zero bool) {
 	}
 }
 
-fn dirname(path string) string {
-	// Empty strings == '.'
-	if path.len == 0 {
-		return '.'
-	}
+fn last_component(name string) int {
+	mut base := 0
+	mut last_slash := false
 
-	mut len := path.len - 1
-	slash := u8(47)
-
-	// strip any trailing slashes; 47 == '/'
-	for len > 0 && path[len] == slash {
-		len--
-	}
-
-	// find start of directory
-	for len > 0 && path[len] != slash {
-		len--
-	}
-
-	// leading slash or no slashes at all
-	if len == 0 {
-		return if path[len] == slash { '/' } else { '.' }
-	} else {
-		// move past separating slashes
-		for len > 0 && path[len] == slash {
-			len--
+	for ch in name {
+		if is_slash(ch) {
+			base++
+		} else {
+			break
 		}
 	}
 
-	len++
+	for i := base; i < name.len; i++ {
+		if is_slash(name[i]) {
+			last_slash = true
+		} else if last_slash {
+			base = i
+			last_slash = false
+		}
+	}
+	return base
+}
 
-	return path[..len]
+fn dirname(path string) string {
+	basename_index := last_component(path)
+
+	return os.dir(path[..basename_index])
 }
