b26640ed031cd1eb94a2e43c4a6c43324f66b449
syrmel <104119569+syrmel@users.noreply.github.com>
2024-01-26 15:51:37 +0100

    Adding the uniq utility (#123)
    * Initial uniq
    
    * Added uniq tests
    
    * Fixed documentation on read_line_until()
    
    * Replace CustomBufferedReader with new vlib version

---
+++ src/uniq/uniq_test.v
@@ -0,0 +1,77 @@
+import common.testing
+import os
+
+const util = 'uniq'
+const platform_util = $if !windows {
+	util
+} $else {
+	'coreutils ${util}'
+}
+
+const cmd = testing.new_paired_command(platform_util, executable_under_test)
+const executable_under_test = testing.prepare_executable(util)
+const temp_dir = testing.temp_folder
+
+const posix_test_data = [
+	'#01 foo0 bar0 foo1 bar1',
+	'#02 bar0 foo1 bar1 foo1',
+	'#03 foo0 bar0 foo1 bar1',
+	'#04',
+	'#05 foo0 bar0 foo1 bar1',
+	'#06 foo0 bar0 foo1 bar1',
+	'#07 bar0 foo1 bar1 foo0',
+]
+const posix_test_path_newline = 'posix_nl.txt'
+const posix_test_path_zeroterm = 'posix_zt.txt'
+
+fn call_for_test(args string) os.Result {
+	res := os.execute('${executable_under_test} ${args}')
+	assert res.exit_code == 0
+	return res
+}
+
+fn test_posix_spec_case_1() {
+	assert cmd.same_results('-c -f 1 posix_nl.txt')
+}
+
+fn test_posix_spec_case_2() {
+	assert cmd.same_results('-d -f 1 posix_nl.txt')
+}
+
+fn test_posix_spec_case_3() {
+	assert cmd.same_results('-u -f 1 posix_nl.txt')
+}
+
+fn test_posix_spec_case_4() {
+	assert cmd.same_results('-d -s 2 posix_nl.txt')
+}
+
+fn test_posix_spec_case_1_zero_term() {
+	assert call_for_test('-c -f 1 posix_zt.txt').output.split('\0').len == 7
+}
+
+fn test_posix_spec_case_2_zero_term() {
+	assert call_for_test('-d -f 1 -z posix_zt.txt').output.split('\0').len == 2
+}
+
+fn test_posix_spec_case_3_zero_term() {
+	assert call_for_test('-u -f 1 -z posix_zt.txt').output.split('\0').len == 6
+}
+
+fn test_posix_spec_case_4_zero_term() {
+	assert call_for_test('-d -s 2 -z posix_zt.txt').output.split('\0').len == 1
+}
+
+fn testsuite_begin() {
+	os.write_file(posix_test_path_newline, posix_test_data.join('\n'))!
+	os.write_file(posix_test_path_zeroterm, posix_test_data.join('\0'))!
+}
+
+fn testsuite_end() {
+	os.rm(posix_test_path_newline)!
+	os.rm(posix_test_path_zeroterm)!
+}
+
+fn test_help_and_version() {
+	cmd.ensure_help_and_version_options_work()!
+}
