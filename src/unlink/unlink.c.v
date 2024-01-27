5aec089d8d197631433126cea5086e7a3c599a7a
Georg <dev@pmgroup.ch>
2024-01-27 19:31:14 +0100

    For Windows, fall back to calling os.rm()

--- src/unlink/unlink.c.v
+++ src/unlink/unlink.c.v
@@ -1,19 +1,20 @@
 import os
 
-#include <sys/stat.h> // #include <signal.h>
 #include <errno.h>
-#include <unistd.h>
-
-$if freebsd || openbsd {
-	#include <sys/sysctl.h>
+$if !windows {
+	#include <unistd.h>
 }
 
 fn C.unlink(&char) int
 
 fn unlink(settings Settings) {
-	err := C.unlink(&char(settings.target.str))
-	if err != 0 {
-		posix_error := os.posix_get_error_msg(C.errno)
-		fail("cannot unlink '${settings.target}': ${posix_error}")
+	$if !windows {
+		err := C.unlink(&char(settings.target.str))
+		if err != 0 {
+			posix_error := os.posix_get_error_msg(C.errno)
+			fail("cannot unlink '${settings.target}': ${posix_error}")
+		}
+	} $else {
+		os.rm(settings.target) or { panic(err) }
 	}
 }
