e455c9247bcc6dcb49787ea5683820b38decd8b4
Georg <dev@pmgroup.ch>
2024-02-11 23:48:38 +0100

    Prevent tests in supported platforms

--- src/tty/tty_test.v
+++ src/tty/tty_test.v
@@ -11,15 +11,17 @@ fn testsuite_begin() {
 	rig.assert_platform_util()
 }
 
-fn testsuite_end() {
-	rig.clean_up()
-}
-
 fn test_help_and_version() {
+	if !supported_platform {
+		return
+	}
 	rig.assert_help_and_version_options_work()
 }
 
 fn test_compare() {
+	if !supported_platform {
+		return
+	}
 	rig.assert_same_results('')
 	rig.assert_same_results('-s')
 	rig.assert_same_results('--silent')
