926d23a983dee2770ccc572190c71476d5eb2826
Georg <dev@pmgroup.ch>
2024-02-17 10:14:00 +0100

    Add exit codes and help to common

--- common/common.v
+++ common/common.v
@@ -1,14 +1,18 @@
 module common
 
 import flag
+import os
 
 pub const version = '0.0.1'
+pub const err_programming_error = 0x7c
+pub const err_not_implemented = 0x7d
 pub const err_platform_not_supported = 0x7F
 
 pub struct CoreutilInfo {
 pub:
 	name        string
 	description string
+	help        string
 }
 
 pub struct CoreutilExitDetail {
@@ -73,12 +77,19 @@ pub fn (app CoreutilInfo) quit(detail CoreutilExitDetail) {
 	exit(detail.return_code)
 }
 
+pub fn (app CoreutilInfo) eprintln_posix(message string) {
+	eprintln('${app.name}: ${message}: ${os.error_posix().msg}')
+}
+
 // flag_parser returns a flag.FlagParser, with the common
 // options already set, reducing the boilerplate code in
 // each individual utility.
 pub fn (app CoreutilInfo) make_flag_parser(args []string) &flag.FlagParser {
 	mut fp := flag.new_flag_parser(args)
 	fp.version(coreutils_version())
+	if app.help != '' {
+		fp.footer(app.help)
+	}
 	fp.footer(coreutils_footer())
 	fp.skip_executable()
 	fp.application(app.name)
