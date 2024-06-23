97c13e37752a0e9385e668fd64631d097fe845b3
mike-ward <mike@mike-ward.net>
2024-06-23 11:38:29 -0500

    update correc test this time

--- src/unexpand/unexpand_test.v
+++ src/unexpand/unexpand_test.v
@@ -12,10 +12,8 @@ fn write_tmp_file(content string) string {
 fn test_1() {
 	file := write_tmp_file('        a       b       c\n')
 	res := os.execute('unexpand ${file}')
-	assert res.output.bytes() == [u8(97), 9, 98, 9, 99, 9, 100, 9, 101, 9, 102, 10]
-
-	// [u8(9), 97, 32, 32, 32, 32, 32, 32, 32, 98, 32, 32, 32, 32, 32,
-	// 	32, 32, 99, 10]
+	assert res.output.bytes() == [u8(9), 97, 32, 32, 32, 32, 32, 32, 32, 98, 32, 32, 32, 32, 32,
+		32, 32, 99, 10]
 }
 
 fn test_2() {
@@ -30,8 +28,10 @@ fn test_2() {
 fn test_3() {
 	file := write_tmp_file('a\tb\tc\td\te\tf\n')
 	res := os.execute('unexpand -t 3,/7 ${file}')
-	assert res.output.bytes() == [u8(117), 110, 101, 120, 112, 97, 110, 100, 58, 32, 98, 97, 100,
-		32, 116, 97, 98, 32, 115, 116, 111, 112, 32, 115, 112, 101, 99, 10]
+	assert res.output.bytes() == [u8(97), 9, 98, 9, 99, 9, 100, 9, 101, 9, 102, 10]
+
+	// [u8(117), 110, 101, 120, 112, 97, 110, 100, 58, 32, 98, 97, 100,
+	// 	32, 116, 97, 98, 32, 115, 116, 111, 112, 32, 115, 112, 101, 99, 10]
 }
 
 fn test_4() {
